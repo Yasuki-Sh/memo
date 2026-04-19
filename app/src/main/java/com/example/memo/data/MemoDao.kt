08bc32a6f76e6c3a76fa11e5d26f0829622d4ef9
Yasuki-Sh <y.s1129@icloud.com>
2026-04-19 17:48:55 +0900

    feat: add MemoDao

---
+++ app/src/main/java/com/example/memo/data/MemoDao.kt
@@ -0,0 +1,24 @@
+package com.example.memo.data
+
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.Query
+import androidx.room.Update
+import com.example.memo.domain.MemoEntity
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface MemoDao {
+    @Insert
+    suspend fun insert(memo: MemoEntity)
+
+    @Query("SELECT * FROM MemoEntity")
+    suspend fun getAll(): Flow<List<MemoEntity>>
+
+    @Update
+    suspend fun update(memo: MemoEntity)
+
+    @Delete
+    suspend fun delete(memo: MemoEntity)
+}
