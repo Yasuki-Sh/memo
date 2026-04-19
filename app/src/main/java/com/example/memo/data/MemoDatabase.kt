109dddce3dea784941b84991bd4a222ea48954f6
Yasuki-Sh <y.s1129@icloud.com>
2026-04-19 23:30:49 +0900

    fix: MemoDatabaseにRoomDatabaseを継承

--- app/src/main/java/com/example/memo/data/MemoDatabase.kt
+++ app/src/main/java/com/example/memo/data/MemoDatabase.kt
@@ -1,9 +1,10 @@
 package com.example.memo.data
 
 import androidx.room.Database
+import androidx.room.RoomDatabase
 import com.example.memo.domain.MemoEntity
 
 @Database(entities = [MemoEntity::class], version = 1)
-abstract class MemoDatabase {
+abstract class MemoDatabase: RoomDatabase() {
     abstract fun memoDao(): MemoDao
 }
